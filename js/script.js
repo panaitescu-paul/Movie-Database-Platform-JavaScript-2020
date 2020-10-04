2e4b0e4fd2905f97578cc6fbbc8073834e5e96b9
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-10-04 15:56:23 +0200

    Ajax API request - get Title, Release Year and Language - JS, HTML, CSS

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ $(document).ready(function() {
 
     // "Search" button clicked
     $("#btnSearch").click(function () {
-        console.log("#searchInput", $("#searchInput").val());
+        let searchInput = $("#searchInput").val().trim();
+        console.log("#searchInput", searchInput);
 
-        if ($("#searchInput").val().trim() == "") {
+        if (searchInput == "") {
             showError("empty");
             return;
         }
@@ -17,7 +18,7 @@ $(document).ready(function() {
         const URL = 'https://api.themoviedb.org/3/search/movie?api_key=' +
             tmdbAPIKey +
             '&language=en-US&query=' +
-            $("#searchInput").val() +
+            searchInput +
             '&page=1&include_adult=false';
 
         // $("$errorMessage").hide();
@@ -28,6 +29,27 @@ $(document).ready(function() {
         })
         .done(function (data) {
             console.log("data", data);
+            console.log("data title", data.results[0].title);
+
+
+            $.each(data.results, function (index1, movie) {
+                const movieItem = $("<article />", {
+                    "id": movie.id
+                });
+                $("<section />", {
+                    "text": "Title: " + movie.title
+                }).appendTo(movieItem);
+
+                $("<section />", {
+                    "text": "Release Year: " + movie.release_date.substring(0,4)
+                }).appendTo(movieItem);
+
+                $("<section />", {
+                    "text": "Language: " + movie.original_language
+                }).appendTo(movieItem);
+
+                movieItem.appendTo($("#movieSearchResult")); // Add to the DOM element
+            });
 
         })
         .fail(function (data) {
