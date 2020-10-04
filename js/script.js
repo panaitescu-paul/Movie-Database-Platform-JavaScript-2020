0f8b3a3f2a63d101faef2bd62c0246d86b203fcc
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-10-04 17:04:47 +0200

    Ajax API request - Search for Person Name, and display list of names - JS

--- js/script.js
+++ js/script.js
@@ -15,8 +15,9 @@ $(document).ready(function() {
             return;
         }
 
-        const URLMovies = 'https://api.themoviedb.org/3/search/movie?api_key=' + tmdbAPIKey +
-            '&language=en-US&query=' + searchInput + '&page=1&include_adult=false';
+        // // Option 1: Search for Movie Title
+        // const URLMovies = 'https://api.themoviedb.org/3/search/movie?api_key=' + tmdbAPIKey +
+        //     '&language=en-US&query=' + searchInput + '&page=1&include_adult=false';
         // const URLpeople = 'https://api.themoviedb.org/3/search/movie?api_key=' + tmdbAPIKey +
         //     '&language=en-US&query=' + searchInput + '&page=1&include_adult=false';
 
@@ -44,53 +45,89 @@ $(document).ready(function() {
         //             "text": "Language: " + movie.original_language
         //         }).appendTo(movieItem);
         //
-        //         movieItem.appendTo($("#movieSearchResult")); // Add to the DOM element
+        //         movieItem.appendTo($("#searchResult")); // Add to the DOM element
         //     });
         //
         // })
         // .fail(function (data) {
         //     showError(data.status.toString());
         // });
-        let movieName = searchInput.slice(0, -5);
-        let movieYear = searchInput.slice(-4);
-        console.log("movieName", movieName);
-        console.log("searchInput", searchInput);
-        console.log("movieYear", movieYear);
 
-        const URLMovies2 = 'https://api.themoviedb.org/3/search/movie?api_key=' + tmdbAPIKey +
-            '&language=en-US&query=' + movieName + '&page=1&include_adult=false';
+
+        // // Option 2: Search for Movie Title and Year
+        // let movieName = searchInput.slice(0, -5);
+        // let movieYear = searchInput.slice(-4);
+        // console.log("movieName", movieName);
+        // console.log("searchInput", searchInput);
+        // console.log("movieYear", movieYear);
+        //
+        // const URLMovies2 = 'https://api.themoviedb.org/3/search/movie?api_key=' + tmdbAPIKey +
+        //     '&language=en-US&query=' + movieName + '&page=1&include_adult=false';
+        // $.ajax({
+        //     url: URLMovies2,
+        //     type: "GET"
+        // })
+        // .done(function (data) {
+        //     console.log("data", data);
+        //     console.log("data title", data.results[0].title);
+        //     $.each(data.results, function (index1, movie) {
+        //         console.log(searchInput.substring(-4));
+        //         if (movieYear == movie.release_date.substring(0,4)) { //check if the Searched Movie has the same Year with the release date
+        //             const movieItem = $("<article />", {
+        //                 "id": movie.id
+        //             });
+        //             $("<section />", {
+        //                 "text": "Title: " + movie.title
+        //             }).appendTo(movieItem);
+        //
+        //             $("<section />", {
+        //                 "text": "Release Year: " + movie.release_date.substring(0,4)
+        //             }).appendTo(movieItem);
+        //
+        //             $("<section />", {
+        //                 "text": "Language: " + movie.original_language
+        //             }).appendTo(movieItem);
+        //
+        //             movieItem.appendTo($("#searchResult")); // Add to the DOM element
+        //         }
+        //     });
+        // })
+        // .fail(function (data) {
+        //     showError(data.status.toString());
+        // });
+
+        // Option 3: Search for Person Name
+        let personName = searchInput;
+        console.log("personName", personName);
+
+        const URLPerson = 'https://api.themoviedb.org/3/search/person?api_key=' + tmdbAPIKey +
+            '&language=en-US&query=' + personName + '&page=1&include_adult=false';
         $.ajax({
-            url: URLMovies2,
+            url: URLPerson,
             type: "GET"
         })
-            .done(function (data) {
-                console.log("data", data);
-                console.log("data title", data.results[0].title);
-                $.each(data.results, function (index1, movie) {
-                    console.log(searchInput.substring(-4));
-                    if (movieYear == movie.release_date.substring(0,4)) { //check if the Searched Movie has the same Year with the release date
-                        const movieItem = $("<article />", {
-                            "id": movie.id
-                        });
-                        $("<section />", {
-                            "text": "Title: " + movie.title
-                        }).appendTo(movieItem);
-
-                        $("<section />", {
-                            "text": "Release Year: " + movie.release_date.substring(0,4)
-                        }).appendTo(movieItem);
-
-                        $("<section />", {
-                            "text": "Language: " + movie.original_language
-                        }).appendTo(movieItem);
-
-                        movieItem.appendTo($("#movieSearchResult")); // Add to the DOM element
-                    }
+        .done(function (data) {
+            console.log("data", data);
+            console.log("data name", data.results[0].name);
+            $.each(data.results, function (index1, person) {
+                console.log(searchInput);
+                const personItem = $("<article />", {
+                    "id": person.id
                 });
-            })
-            .fail(function (data) {
-                showError(data.status.toString());
+                $("<section />", {
+                    "text": "Name: " + person.name
+                }).appendTo(personItem);
+
+                $("<section />", {
+                    "text": "Main Activity: " + person.known_for_department
+                }).appendTo(personItem);
+
+                personItem.appendTo($("#searchResult")); // Add to the DOM element
             });
+        })
+        .fail(function (data) {
+            showError(data.status.toString());
+        });
     })
 
     // TODO: implement
